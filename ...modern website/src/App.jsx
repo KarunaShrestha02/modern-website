99a3dfe8123b38df5a0aead9352fb2cf4e5850ca
KarunaShrestha02 <karuna100502@gmail.com>
2024-06-29 18:51:11 +0545

    no message

--- ...modern website/src/App.jsx
+++ ...modern website/src/App.jsx
@@ -1,15 +1,15 @@
 import styles from "./style.js";
 //import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
-import './components/Billing.jsx';
-import './components/Business.jsx';
-import './components/CardDeal.jsx';
-import './components/Clients.jsx';
-import './components/CTA.jsx';
-import './components/Footer.jsx';
-import './components/Navbar.jsx';
-import './components/Stats.jsx';
-import './components/Testimonials.jsx';
-import './components/Hero.jsx';
+import Billing from './components/Billing.jsx';
+import Business from './components/Business.jsx';
+import  CardDeal from './components/CardDeal.jsx';
+import Clients from './components/Clients.jsx';
+import CTA from './components/CTA.jsx';
+import Footer from './components/Footer.jsx';
+import Navbar from "./components/Navbar.jsx";
+import Stats from './components/Stats.jsx';
+import Testimonials from './components/Testimonials.jsx';
+import Hero from './components/Hero.jsx';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
